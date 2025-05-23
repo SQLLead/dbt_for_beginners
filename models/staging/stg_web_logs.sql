2d6366053870f9b18442505bba4e37eb020b9d4c
ericbragas@protonmail.com <ericbragas@protonmail.com>
2025-05-23 22:18:59 +0000

    Added incremental loads.

---
+++ models/staging/stg_web_logs.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='view') }}
+
+select
+    {{ dbt_utils.generate_surrogate_key(['user_id', 'page', 'event_type', 'event_timestamp']) }} as event_id,
+    cast(user_id as integer) as user_id,
+    page,
+    event_type,
+    cast(event_timestamp as timestamp) as event_timestamp
+from (
+    select * from {{ ref('web_logs_day1') }}
+    union all
+    select * from {{ ref('web_logs_day2') }}
+    union all
+    select * from {{ ref('web_logs_day3') }}
+) as unioned_logs
